e8371444ba7f16666d217119f4d17003771cf9a8
PhantomDio <diodjsm@gmail.com>
2023-05-27 01:03:28 -0600

    ClientesDAO vFinal

--- Paseos_El_Puerto/src/main/java/datos/ClientesDAO.java
+++ Paseos_El_Puerto/src/main/java/datos/ClientesDAO.java
@@ -4,18 +4,42 @@ import model.Clientes;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-//import java.util.StringTokenizer;
 
 public class ClientesDAO {
-    public static final String selectSQL = "SELECT * FROM clientes";
-    public static final String deleteSQL = "DELETE FROM clientes WHERE id_cliente = ? ";
 
+    public void select(int id) {
+        String selectSQL = "SELECT * FROM clientes WHERE id_cliente=?";
+        try {
+            PreparedStatement statement = Conexion.getConnection().prepareStatement(selectSQL);
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                System.out.println("ID: " + rs.getInt("id_cliente"));
+                System.out.println("Nombre: " + rs.getString("nombre"));
+                System.out.println("Apellido paterno: " + rs.getString("ap_pat"));
+                System.out.println("Apellido materno: " + rs.getString("ap_mat"));
+                System.out.println("Edad: " + rs.getInt("edad"));
+                System.out.println("Fecha de nacimiento: " + rs.getDate("f_nac"));
+                System.out.println("Celular: " + rs.getString("cel"));
+                System.out.println("Teléfono: " + rs.getString("tel"));
+                System.out.println("Correo electrónico: " + rs.getString("email"));
+                System.out.println("Dirección: " + rs.getString("direccion"));
 
-    public List<Clientes> seleccionar(){
+            } else {
+                System.out.println("No se encontró ningún alumno con el ID " + id);
+            }
+            statement.close();
+            rs.close();
+        } catch (SQLException ex) {
+            System.out.println("Error al seleccionar alumno: " + ex.getMessage());
+        }
+    }
+    public List<Clientes> selectAll(){
         Connection conn = null;
         Statement state = null;
         ResultSet result = null;
         Clientes client = null;
+        String selectSQL = "SELECT * FROM clientes";
 
         List<Clientes> clientes = new ArrayList<>();
         try {
@@ -31,8 +55,9 @@ public class ClientesDAO {
                 String direccion = result.getString("direccion");
                 String telefono = result.getString("telefono");
                 String email = result.getString("email");
+                Date fecha_nac = result.getDate("fecha_nac");
 
-                client = new Clientes(id_cliente,nombre,apellido_Pat,apellido_Mat, direccion,telefono,email);
+                client = new Clientes(id_cliente,nombre,apellido_Pat,apellido_Mat, direccion,telefono,email, fecha_nac);
                 clientes.add(client);
 
             }
@@ -44,8 +69,8 @@ public class ClientesDAO {
             for(Clientes cliente: clientes) {
                 System.out.println("ID_cliente: " + cliente.getIdCliente());
                 System.out.println("Nombre: " + cliente.getNombre());
-                System.out.println("Apellido_Pat: " + cliente.getApellido_Pat());
-                System.out.println("Apellido_Mat: " + cliente.getApellido_Mat());
+                System.out.println("Apellido_Pat: " + cliente.getApellidoP());
+                System.out.println("Apellido_Mat: " + cliente.getApellidoM());
                 System.out.println("Direccion: " + cliente.getDireccion());
                 System.out.println("Telefono: " + cliente.getTelefono());
                 System.out.println("Email: " + cliente.getEmail());
@@ -67,8 +92,8 @@ public class ClientesDAO {
             PreparedStatement ps = con.prepareStatement(query);
             ps.setInt(indexID, cliente.getIdCliente());
             ps.setString(indexNOMBRE, cliente.getNombre());
-            ps.setString(indexAPELLP, cliente.getApellido_Pat());
-            ps.setString(indexAPELLM, cliente.getApellido_Mat());
+            ps.setString(indexAPELLP, cliente.getApellidoP());
+            ps.setString(indexAPELLM, cliente.getApellidoM());
             ps.setString(indexEDAD, cliente.getDireccion());
             ps.setString(indexCEL, cliente.getTelefono());
             ps.setString(indexEMAIL, cliente.getEmail());
@@ -110,31 +135,16 @@ public class ClientesDAO {
         }
     }
 
-    public int borrar(Clientes cliente){
-
-        Connection conn = null;
-        PreparedStatement state = null;
-        int registros=0;
-
-        try{
-            conn = Conexion.getConnection();
-            state = conn.prepareStatement(deleteSQL);
-
-            state.setInt(1,cliente.getIdCliente());
-            registros = state.executeUpdate();
-
-            if(registros>0)
-                System.out.println("Registro eliminado");
-
-
-            Conexion.close(state);
-            Conexion.close(conn);
-            Clientes clienteDelete = new Clientes();
-
-        }catch (Exception e) {
-            e.printStackTrace();
+    public void delete(int id) {
+        String deleteSQL = "DELETE FROM clientes WHERE id_cliente=?";
+        try {
+            PreparedStatement statement = Conexion.getConnection().prepareStatement(deleteSQL);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+            statement.close();
+            System.out.println("Registro eliminado exitosamente.");
+        } catch (SQLException ex) {
+            System.out.println("Error al eliminar el alumno: " + ex.getMessage());
         }
-
-        return registros;
     }
 }
