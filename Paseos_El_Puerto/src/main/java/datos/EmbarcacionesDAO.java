72ec879d3f8d5a005d92e262cb1d17ab658138e8
PhantomDio <diodjsm@gmail.com>
2023-06-03 14:24:04 -0600

    Terminando operaciones CRUD de "Embarcaciones" con relación a otras tablas

--- Paseos_El_Puerto/src/main/java/datos/EmbarcacionesDAO.java
+++ Paseos_El_Puerto/src/main/java/datos/EmbarcacionesDAO.java
@@ -8,19 +8,27 @@ import java.util.ArrayList;
 public class EmbarcacionesDAO {
 
     public Embarcaciones select(int id) {
-        String selectSQL = "SELECT * FROM embarcaciones WHERE id_embarcacion=?";
+        String selectSQL = "SELECT e.id_embarcacion, e.nombre, e.modelo, e.longitud, e.anio, " +
+                "CONCAT(p.nombre, ' ', p.ap_pat, ' ', p.ap_mat) AS nombre_propietario, c.fecha_fin AS fecha_fin_contrato, " +
+                "c.costo_hora, ps.fecha_fin AS fecha_fin_paseo FROM embarcaciones e INNER JOIN propietarios p " +
+                "ON e.id_propietario = p.id_propietario LEFT JOIN contratos c ON e.id_embarcacion = c.id_embarcacion " +
+                "LEFT JOIN paseos ps ON e.id_embarcacion = ps.id_embarcacion";
         try {
             PreparedStatement statement = Conexion.getConnection().prepareStatement(selectSQL);
             statement.setInt(1, id);
             ResultSet rs = statement.executeQuery();
             Embarcaciones embar = new Embarcaciones();
             while (rs.next()) {
-
                 embar.setIdEmbarcacion(rs.getInt("id_embarcacion"));
                 embar.setNombre(rs.getString("nombre"));
                 embar.setModelo(rs.getString("modelo"));
                 embar.setLongitud(rs.getFloat("longitud"));
                 embar.setAnio(rs.getInt("anio"));
+
+                embar.setnombreProp(rs.getString("nombre_propietario"));
+                embar.setFechaFinContrato(rs.getDate("fecha_fin_contrato"));
+                embar.setCostoHora(rs.getFloat("costo_hora"));
+                embar.setFechaFinPaseo(rs.getDate("fecha_fin_paseo"));
             }
             statement.close();
             rs.close();
